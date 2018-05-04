1db2954905107d2408aa0ddd8ec21d5923a8091f
Johnie Xu <281910378@qq.com>
2018-05-04 23:52:06 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  title: "JohnieXu's Blog",
+  description: 'Believe in yourself and just do it! Go......',
+  base: '/xx-blog/',
+  dest: 'dist',
   repo: 'https://github.com/JohnieXu',
   themeConfig: {
     nav: [
