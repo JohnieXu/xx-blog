8fc56a7789c070747c09fcc3dc36f9953913affc
JohnieXu <281910378@qq.com>
2020-03-06 00:48:41 +0800

    chore: 默认部署至根路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,7 @@ const OSS_BASE_PATH = 'https://lessing.oss-cn-beijing.aliyuncs.com/blog/'
 const CODING_BASE_PATH = '/'
 let PUBLISH_PATH = '/xx-blog/'
 
-PUBLISH_PATH = OSS_BASE_PATH // 默认部署阿里云oss
+PUBLISH_PATH = CODING_BASE_PATH // 默认根路径
 
 module.exports = {
     title: "迪诺笔记",
